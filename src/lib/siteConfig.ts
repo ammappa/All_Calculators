e5dbaae7cf019924b4e3121f76278d1331752fbe
M Talha <talha18513@gmail.com>
2026-04-20 07:27:10 +0500

    add calculator link

--- src/lib/siteConfig.ts
+++ src/lib/siteConfig.ts
@@ -11,7 +11,7 @@ export const siteConfig = {
     contactPhoneDisplay: "",
     businessProfileUrl:
         process.env.NEXT_PUBLIC_GOOGLE_BUSINESS_PROFILE_URL?.trim() ||
-        "https://www.google.com/maps/search/?api=1&query=WithinSecs",
+        "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3941.509813515119!2d77.5505332!3d8.925078699999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b0421d001e916cd%3A0xc50082f28694b351!2sKadanganeri%2C%20Tamil%20Nadu%20627854%2C%20India!5e0!3m2!1sen!2s!4v1776651965087!5m2!1sen!2s",
     mapsEmbedUrl:
         "https://www.google.com/maps?q=WithinSecs&output=embed",
     serviceArea: "Worldwide",
